8ce99bf41838cc8acf9cad19fca37ce235ee5e32
Christoph Keller <christoph.a.keller@nasa.gov>
2021-02-12 09:23:13 -0500

    Add option to plot bias ts

--- cfboost/plot.py
+++ cfboost/plot.py
@@ -33,7 +33,7 @@ def plot_pred_vs_obs(modtable,obs,**kwargs):
     return
 
 
-def _plot_ts(modtable,obs,iloc,ofile_template='mod_vs_obs_%l.png',sample_freq='1D',loccol='original_station_name',titlecol=None):
+def _plot_ts(modtable,obs,iloc,ofile_template='mod_vs_obs_%l.png',sample_freq='1D',loccol='original_station_name',titlecol=None,**kwargs):
     '''Plot predictions vs observations.'''
     log = logging.getLogger(__name__)
     idat = modtable.loc[modtable['Location']==iloc]
@@ -64,7 +64,7 @@ def _plot_ts(modtable,obs,iloc,ofile_template='mod_vs_obs_%l.png',sample_freq='1
         ldat = _check_units(ldat,ispec,munit)
         ldat = ldat.groupby('ISO8601').mean().resample(sample_freq).mean().reset_index()
         ax = fig.add_subplot(nrow,ncol,irow+1)
-        _plot_single_ts(ax,ldat,ispec,munit,origvar,mlvar)
+        _plot_single_ts(ax,ldat,ispec,munit,origvar,mlvar,**kwargs)
         cnt += 1
     # clean up
     fig.tight_layout(rect=[0, 0.03, 1, 0.95])
@@ -76,7 +76,7 @@ def _plot_ts(modtable,obs,iloc,ofile_template='mod_vs_obs_%l.png',sample_freq='1
     return
 
 
-def _plot_single_ts(ax,ldat,ispec,munit,origvar,mlvar,obsvar='value',title=None):
+def _plot_single_ts(ax,ldat,ispec,munit,origvar,mlvar,obsvar='value',title=None,bias=False,label1='Model',label2='Model + ML',label3='Observation'):
     xlocs = [dt.datetime(2018,1,1),dt.datetime(2019,1,1),dt.datetime(2020,1,1)]
     xlabs = [i.strftime('%Y') for i in xlocs].copy()
     xticks = []
@@ -85,32 +85,49 @@ def _plot_single_ts(ax,ldat,ispec,munit,origvar,mlvar,obsvar='value',title=None)
         xticks.append(idate)
         idate = idate + dt.timedelta(days=35)
         idate = dt.datetime(idate.year,idate.month,1)
-    ax.plot(ldat.ISO8601,ldat[origvar],color='dimgray',linewidth=2,label='Model',alpha=0.75)
-    ax.plot(ldat.ISO8601,ldat[mlvar],color='black',linewidth=2,alpha=0.75,label='Model + ML')
-    ax.plot(ldat.ISO8601,ldat['value'],color='red',linewidth=2,alpha=0.75,label='Observation')
-    ax.legend(loc='upper left',ncol=3,frameon=False,bbox_to_anchor=(0.0,1.00))
+    # select data to plot
+    xdat = np.array(ldat.ISO8601)
+    ref = ldat[origvar]-ldat['value'] if bias else ldat[origvar]
+    ml  = ldat[mlvar]-ldat['value'] if bias else ldat[mlvar]
+    obs = None if bias else ldat['value']
+    col1 = 'black' if bias else 'dimgray'
+    col2 = 'red' if bias else 'black'
+    col3 = 'red'
+    # plot
+    if bias:
+        plt.axhline(0.0,color='black')
+    ax.plot(xdat,np.array(ref),color=col1,linewidth=2,label=label1,alpha=0.75)
+    ax.plot(xdat,np.array(ml),color=col2,linewidth=2,alpha=0.75,label=label2)
+    ncols=2
+    if obs is not None:
+        ax.plot(xdat,np.array(obs),color=col3,linewidth=2,alpha=0.75,label=label3)
+        ncols+=1
+    ax.legend(loc='upper left',ncol=ncols,frameon=False,bbox_to_anchor=(0.0,1.00))
     ax.set_xlim([min(xlocs),max(xlocs)])
     ax.set_xticks(xticks,minor=True)
     ax.set_xticks(xlocs)
     ax.set_xticklabels(xlabs)
     unitl = '[$\mu$gm$^{-3}$]' if 'ugm-3' in munit else '[ppbv]'
     if ispec=='no2':
-        ylabel = 'NO$_{2}$ '+unitl
+        ylabel = 'NO$_{2}$ '
         defmax = 40.0
     if ispec=='o3':
-        ylabel = 'O$_{3}$ '+unitl
+        ylabel = 'O$_{3}$ '
         defmax = 80.0
     if 'pm25' in ispec:
-        ylabel = 'PM$_{2.5}$ '+unitl
+        ylabel = 'PM$_{2.5}$ '
         defmax = 100.0
-    origmax = np.nanmax(ldat[origvar].values)
+    ylabel = ylabel+'bias '+unitl if bias else ylabel+unitl
+    origmax = np.nanmax(np.abs(np.array(ref)))
     origmax = defmax if np.isnan(origmax) else origmax
-    mlmax = np.nanmax(ldat[mlvar].values)
+    mlmax = np.nanmax(np.abs(np.array(ml)))
     mlmax = defmax if np.isnan(mlmax) else mlmax
-    obsmax = np.nanmax(ldat['value'].values)
-    obsmax = defmax if np.isnan(obsmax) else obsmax
-    maxval = np.max([origmax,mlmax,obsmax])
-    ylim = [0,maxval]
+    if obs is not None:
+        obsmax = np.nanmax(np.array(obs))
+        obsmax = defmax if np.isnan(obsmax) else obsmax
+    maxval = np.max([origmax,mlmax]) if bias else np.max([origmax,mlmax,obsmax])
+    minval = maxval*-1. if bias else 0.0
+    ylim = [minval,maxval]
     ax.set_ylim(ylim)
     ax.set_ylabel(ylabel)
     if title is not None:
